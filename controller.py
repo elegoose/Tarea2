ad5dca0aa8c8f94e4721c3653493011be85c3c55
Goose <60141816+elegoose@users.noreply.github.com>
2021-02-11 00:44:54 -0300

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Controller:
+    def __init__(self):
+        self.camaratheta = 0
+        self.r = 5
+        self.camx = 1
+        self.camy = 0
+        self.camz = 0
+        self.followbody = False
+        self.bodyID = 0
+
+    def update(self):
+        self.camx = self.r * np.cos(self.camaratheta)
+        self.camy = self.r * np.sin(self.camaratheta)
