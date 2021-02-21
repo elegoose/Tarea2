5e449097e828458f8167d0c64706b01da6c95bc1
Goose <60141816+elegoose@users.noreply.github.com>
2021-02-21 06:56:47 -0300

    arreglos a camara

--- controller.py
+++ controller.py
@@ -11,10 +11,7 @@ class Controller:
         self.followbody = False
         self.bodyID = 0
         self.maxbodyID = 0
-        self.fillPolygon = True
-        self.printinfo = False
-        self.mover = 0.1
-        self.movea = 0
+        self.icon = None
 
     def update(self):
         self.camx = self.r * np.cos(self.camaratheta)
@@ -28,35 +25,42 @@ def camerapos(bodies, controller):
         if controller.bodyID == 0:
             camx = 0.73
             camy = 0.12
-            camz = 0.42
+            camz = 0.16
             eye = np.array([camx, camy, camz])
             viewPos = np.array([0, 0, 0])
             return viewPos, eye
-        elif controller.bodyID == body.bodyID:
-            # theta = body.theta + controller.movea
-            # cameradistance = body.distance + controller.mover
-            camtheta = - 0.65
-            cameradistance = body.distance + 0.019
-            eyeposx = cameradistance * np.cos(body.theta+camtheta)
-            eyeposy = cameradistance * np.sin(body.theta+camtheta)/np.sqrt(1+body.inclination ** 2)
+        elif body.model == 'models/ufo.obj' and controller.bodyID == body.bodyID:
+            camtheta = -0.24
+            cameradistance = body.distance + 0.1
+            eyeposx = cameradistance * np.cos(body.theta + camtheta)
+            eyeposy = cameradistance * np.sin(body.theta + camtheta) / np.sqrt(1 + body.inclination ** 2)
             eyeposz = eyeposy * body.inclination
 
             viewPos = np.array([body.posx, body.posy, body.posz])
-            eye = np.array([eyeposx, eyeposy, eyeposz+0.05])
+            eye = np.array([eyeposx, eyeposy, eyeposz + 0.24])
+            return viewPos, eye
+        elif controller.bodyID == body.bodyID:
+            camtheta = - 0.65
+            cameradistance = body.distance + 0.019
+            eyeposx = cameradistance * np.cos(body.theta + camtheta)
+            eyeposy = cameradistance * np.sin(body.theta + camtheta) / np.sqrt(1 + body.inclination ** 2)
+            eyeposz = eyeposy * body.inclination
+
+            viewPos = np.array([body.posx, body.posy, body.posz])
+            eye = np.array([eyeposx, eyeposy, eyeposz + 0.05])
             return viewPos, eye
         elif body.satellites != 'Null':
             for satellite in body.satellites:
                 if controller.bodyID == satellite.bodyID:
-                    # theta = satellite.theta + controller.movea
-                    # cameradistance = satellite.distance + controller.mover
-                    camtheta =  - 0.76
+                    camtheta = - 0.76
                     cameradistance = satellite.distance + 0.01
                     eyeposx = cameradistance * np.cos(satellite.theta + camtheta) + body.posx
-                    eyeposy = cameradistance * np.sin(satellite.theta + camtheta)/np.sqrt(1+satellite.inclination ** 2) + body.posy
-                    eyeposz = cameradistance * satellite.inclination * np.sin(satellite.theta + camtheta)/np.sqrt(1+satellite.inclination ** 2) + body.posz
-                    eye = np.array([eyeposx, eyeposy, eyeposz+0.05])
-                    viewPos = np.array([satellite.posx+body.posx, satellite.posy+body.posy, satellite.posz+body.posz])
-                    # viewPos = np.array([eyeposx, eyeposy, eyeposz])
-                    # return eye, viewPos
+                    eyeposy = cameradistance * np.sin(satellite.theta + camtheta) / np.sqrt(
+                        1 + satellite.inclination ** 2) + body.posy
+                    eyeposz = cameradistance * satellite.inclination * np.sin(satellite.theta + camtheta) / np.sqrt(
+                        1 + satellite.inclination ** 2) + body.posz
+                    eye = np.array([eyeposx, eyeposy, eyeposz + 0.05])
+                    viewPos = np.array(
+                        [satellite.posx + body.posx, satellite.posy + body.posy, satellite.posz + body.posz])
                     return viewPos, eye
     return viewPos, eye
